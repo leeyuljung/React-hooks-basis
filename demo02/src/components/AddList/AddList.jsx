9579a5311bc893d25fec8f2d0b0020301f374f83
leeyuljung <rita810605@gmail.com>
2020-12-24 18:31:42 +0800

    demo02: add shouldComponentUpdate to fix the problem of  AddList component rendering too frequently

--- demo02/src/components/AddList/AddList.jsx
+++ demo02/src/components/AddList/AddList.jsx
@@ -13,15 +13,26 @@ class AddList extends Component {
     this.props.deleteItem(this.props.index)
   }
 
+  // 為解決性能問題，當父組件產生變化時，子組件不需要一直重新 render
+  shouldComponentUpdate(nextProps, nextState){
+    // 當子組件更新後的 props 和原先的不一樣時，再去渲染就好
+    if(nextProps.content !== this.props.content){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   UNSAFE_componentWillReceiveProps(){
-    console.log('Updation---componentWillReceiveProps---子組件接收到父組件傳遞來的參數');
+    // console.log('Updation---componentWillReceiveProps---子組件接收到父組件傳遞來的參數');
   }
 
   componentWillUnmount(){
-    console.log('UnMounting---componentWillUnmount---組件被刪除的時候');
+    // console.log('UnMounting---componentWillUnmount---組件被刪除的時候');
   }
 
   render() { 
+    console.log('child rendering');
     let content = this.props.content;
     let name = this.props.name;
     return ( 
