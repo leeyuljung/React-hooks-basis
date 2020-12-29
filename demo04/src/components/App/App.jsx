ff3a298a0738b0e4512983cf1d64afcc6dbc4fbb
leeyuljung <rita810605@gmail.com>
2020-12-29 18:26:54 +0800

    demo04: Change functional components into class components

--- demo04/src/components/App/App.jsx
+++ demo04/src/components/App/App.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Index } from '../Index';
+import { List } from '../List';
 
-class App extends Component {
-  render() { 
-    return ( 
-      <div>test</div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/list">List</Link></li>
+    </ul>
+    <Route path="/" exact component={Index} />
+    <Route path="/list" component={List} />
+  </Router>
+)
  
 export { App } ;
---
+++ demo04/src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() { 
+    return ( 
+      <div>This is List-Page.</div>
+    );
+  }
+}
+ 
+export { List };
