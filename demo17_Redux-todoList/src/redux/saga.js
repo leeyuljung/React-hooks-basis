e14c17953cd40187c5d59a7e67b4fdbd7d427d66
leeyuljung <rita810605@gmail.com>
2021-01-19 15:08:47 +0800

    demo17: Install and configure redux-saga

---
+++ demo17_Redux-todoList/src/redux/saga.js
@@ -0,0 +1,3 @@
+function* mySaga(){}
+
+export default mySaga;
--- demo17_Redux-todoList/src/redux/store/index.js
+++ demo17_Redux-todoList/src/redux/store/index.js
@@ -1,15 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from '../reducer';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import mySaga from '../saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk))
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
 
 const store = createStore(
   reducer,
   enhancer
 );
+
+sagaMiddleware.run(mySaga);
 
 export default store;
